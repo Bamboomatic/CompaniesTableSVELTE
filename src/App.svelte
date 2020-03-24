b1a2f22c3c5b6cf2f1018a6fab1ff5e097950d5b
Bamboomatic <bambynek.pawel@gmail.com>
2020-03-24 21:48:31 +0100

    empty html table template

--- src/App.svelte
+++ src/App.svelte
@@ -24,11 +24,48 @@
   }
 </style>
 
+<header>
+  <h1>COMPANIES INCOME TABLE</h1>
+</header>
+
+<nav id="nav">
+  <input type="text" id="input" placeholder="filter by name" value="" />
+</nav>
+
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <table>
+    <thead>
+      <th>id</th>
+      <th>city</th>
+      <th>name</th>
+      <th>total income</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Loading...</td>
+        <td />
+        <td />
+        <td />
+      </tr>
+    </tbody>
+  </table>
 </main>
+
+<footer>1 2 3 4 5 ... pagination pages buttons</footer>
+
+<div id="modal" class="modal">
+  <div class="modal-content">
+    <span id="closeBtn" class="closeBtn">&times;</span>
+    <div id="modal-data" class="modal-data">Data to be loaded</div>
+    <!-- <form id="range">
+      from:
+      <input type="month" name="start" value="2019-01" id="startDate" />
+      to:
+      <input type="month" name="end" value="2020-01" id="endDate" />
+      <input type="submit" value="Submit form" />
+    </form> -->
+    <canvas id="myChart" />
+  </div>
+</div>
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3"></script> -->
